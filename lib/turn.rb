4c8fb4bd53cbfc1b10bb633c31b9cdfe4a5b6c50
Elliot Blanchard <public@elliotblanchard.com>
2020-04-06 14:06:46 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,6 +2,11 @@
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board,index)
+    move(board,index,character="X")
+  else turn(board)
+  end
 end
 
 #display_board
